34c5266a6de27d8336e5728f7bf11e6f340b5e7b
fatehullah778866 <fatehmsd2522004@gmail.com>
2025-09-02 21:42:06 +0500

    modified the script.js

--- class-1/script.js
+++ class-1/script.js
@@ -13,3 +13,53 @@ introduce: function(){
 
 document.write(student.name);
 student.introduce();
+
+
+// using new object()
+
+const teacher = new Object();
+teacher.name = "Mr. Khan";
+teacher.subject = "Math";
+teacher.teach = function(){
+    document.write("Teaching " + this.subject);
+};
+
+teacher.teach();
+
+// constructor function
+
+function Car(model, year){
+    this.model = model;
+    this.year = year;
+    this.drive = function(){
+        document.write("Driving a " + this.year + " " + this.model);
+    };
+}
+
+const car1 = new Car("Toyota", 2022);
+const car2 = new Car("Honda", 2023);
+
+car1.drive();
+car2.drive();
+
+
+
+
+// ES6 classes
+
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(){
+        document.write("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
+    }
+}
+
+const person1 = new Person("Alice", 30);
+const person2 = new Person("Bob", 25);
+person1.greet();
+person2.greet();
+
